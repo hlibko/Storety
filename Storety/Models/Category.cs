d2140d5d7c5ed889ceadb34a78e1edda521c5d69
Hlib Kochubei <hlib.kochubei@gmail.com>
2023-07-08 13:56:14 +0200

    Add 'Category' model

---
+++ Storety/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storety.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public int DispayOrder { get; set; } = 0;
+    }
+}
