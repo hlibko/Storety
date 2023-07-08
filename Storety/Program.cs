546365eebd7836be1f00372181816d8f6d3b9474
Hlib Kochubei <hlib.kochubei@gmail.com>
2023-07-08 14:46:26 +0200

    Add connection strings & update database

--- Storety/Program.cs
+++ Storety/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Storety.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connString = builder.Configuration.GetConnectionString("PgsqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connString));
 
 var app = builder.Build();
 
